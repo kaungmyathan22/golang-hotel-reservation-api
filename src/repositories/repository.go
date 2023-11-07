0e9af52bfc6e1e57fba945fc3c48a07447e4ee0e
Kaung Myat Han <arkarhtethan@gmail.com>
2023-11-07 16:14:55 +0630

    update(): generic Store type added

--- src/repositories/repository.go
+++ src/repositories/repository.go
@@ -5,3 +5,10 @@ const (
 	TEST_DB_NAME = "test-hotel-reservation"
 	DB_URI       = "mongodb://localhost:27017"
 )
+
+type Store struct {
+	User  UserStore
+	Hotel HotelStore
+	Room  RoomStore
+	Book  BookingStore
+}
